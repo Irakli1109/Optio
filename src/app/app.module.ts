b1a836963a82453dff49bbcd64349265c2a96ad9
Irakli Turabelidze <turabelidze.irakli@gmail.com>
2022-02-09 21:48:03 +0400

    Added styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnalyticsComponent } from './analytics/analytics.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
