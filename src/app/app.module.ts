4c8b73c2b260dfbe4f50e2346f97612944ea66a8
Irakli Turabelidze <turabelidze.irakli@gmail.com>
2022-02-09 21:23:15 +0400

    Set up Folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AnalyticsComponent
   ],
   imports: [
     BrowserModule,
